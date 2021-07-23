3066d334722a6269179f3b3e1c51565ad65e26f2
Melody Wen <40778015+melo2902@users.noreply.github.com>
2021-07-23 12:49:33 -0400

    Update XML Form Pods

--- Pods/Target Support Files/Pods-fbu-app-fbu-appUITests/Pods-fbu-app-fbu-appUITests-resources.sh
+++ Pods/Target Support Files/Pods-fbu-app-fbu-appUITests/Pods-fbu-app-fbu-appUITests-resources.sh
@@ -98,11 +98,9 @@ EOM
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/Parse/Parse.bundle"
-  install_resource "${PODS_ROOT}/XLForm/XLForm/XLForm.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/Parse/Parse.bundle"
-  install_resource "${PODS_ROOT}/XLForm/XLForm/XLForm.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
