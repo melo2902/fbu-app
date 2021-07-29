c1ff8381aecc22e4853d71403064726ce239dfee
Melody Wen <40778015+melo2902@users.noreply.github.com>
2021-07-29 13:52:17 -0400

    Remove navigation bar from settings view controller

--- fbu-app/View Controllers/MTDSettingsViewController.m
+++ fbu-app/View Controllers/MTDSettingsViewController.m
@@ -24,6 +24,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self initializeStates];
+    
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage new]
+                                 forBarMetrics:UIBarMetricsDefault];
+    self.navigationController.navigationBar.shadowImage = [UIImage new];
+    self.navigationController.navigationBar.translucent = YES;
+    self.navigationController.view.backgroundColor = [UIColor clearColor];
+    self.navigationController.navigationBar.backgroundColor = [UIColor clearColor];
 }
 
 - (void) initializeStates {
