3802e1a7420e5c482334acd3d0a8796993e08c3e
Melody Wen <40778015+melo2902@users.noreply.github.com>
2021-08-03 12:08:20 -0400

    Space between message cells

--- fbu-app/Views/MTDConversationCell.m
+++ fbu-app/Views/MTDConversationCell.m
@@ -21,6 +21,20 @@
     [super setSelected:selected animated:animated];
 }
 
+- (void)layoutSubviews {
+    self.contentView.backgroundColor = [UIColor systemGroupedBackgroundColor];
+
+    UIView *whiteRoundedView = [[UIView alloc]initWithFrame:CGRectMake(0, 5, self.contentView.frame.size.width - 1, self.contentView.frame.size.height - 12)];
+                                      
+    whiteRoundedView.layer.backgroundColor = [UIColor whiteColor].CGColor;
+    whiteRoundedView.clipsToBounds = true;
+    whiteRoundedView.layer.masksToBounds = true;
+    whiteRoundedView.layer.cornerRadius = 12.0;
+
+    [self.contentView addSubview:whiteRoundedView];
+    [self.contentView sendSubviewToBack:whiteRoundedView];
+}
+
 - (IBAction)onTapStatusButton:(id)sender {
     if (self.statusButton.selected) {
         NSLog(@"Button is not selected!");
