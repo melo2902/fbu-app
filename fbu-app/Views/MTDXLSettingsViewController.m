26a208d95ac73d8118bf56e3600e851c8644e50e
Melody Wen <40778015+melo2902@users.noreply.github.com>
2021-08-04 12:07:23 -0400

    Connect to GroupMe Webkit

--- fbu-app/Views/MTDXLSettingsViewController.m
+++ fbu-app/Views/MTDXLSettingsViewController.m
@@ -12,8 +12,10 @@
 #import "SceneDelegate.h"
 #import "MTDLoginViewController.h"
 #import "MTDAPIManager.h"
+#import "MTDPlatform.h"
+#import "WebKit/WebKit.h"
 
-@interface MTDXLSettingsViewController ()
+@interface MTDXLSettingsViewController () <UINavigationControllerDelegate, WKNavigationDelegate>
 
 @end
 
@@ -70,10 +72,6 @@
     return 30;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-}
-
 - (void)initializeForm {
     XLFormDescriptor * form;
     XLFormSectionDescriptor * section;
@@ -89,10 +87,11 @@
     [form addFormSection:section];
     
     if (![MTDAPIManager returnAuthToken]) {
-        row = [XLFormRowDescriptor formRowDescriptorWithTag:@"version" rowType:XLFormRowDescriptorTypeSelectorPush title:@"GroupMe"];
-        [row.cellConfig setObject:[UIFont fontWithName:@"Avenir Book" size:16] forKey:@"textLabel.font"];
-        row.action.viewControllerStoryboardId = @"InitialFilterViewController";
-        [section addFormRow:row];
+        XLFormRowDescriptor * buttonLeftAlignedRow = [XLFormRowDescriptor formRowDescriptorWithTag:@"buttonLeftAligned" rowType:XLFormRowDescriptorTypeButton title:@"GroupMe"];
+        [buttonLeftAlignedRow.cellConfig setObject:@(NSTextAlignmentNatural) forKey:@"textLabel.textAlignment"];
+        [buttonLeftAlignedRow.cellConfig setObject:[UIFont fontWithName:@"Avenir Book" size:16] forKey:@"textLabel.font"];
+        buttonLeftAlignedRow.action.formSelector = @selector(connectToGroupMe:);
+        [section addFormRow:buttonLeftAlignedRow];
     } else {
         row = [XLFormRowDescriptor formRowDescriptorWithTag:@"version" rowType:XLFormRowDescriptorTypeSelectorPush title:@"GroupMe"];
         [row.cellConfig setObject:[UIFont fontWithName:@"Avenir Book" size:16] forKey:@"textLabel.font"];
@@ -173,6 +172,66 @@
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
     MTDLoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
     sceneDelegate.window.rootViewController = loginViewController;
+}
+
+# pragma mark - connect to GroupMe
+- (void) connectToGroupMe:(id) sender {
+    NSLog(@"Do we hit this");
+    NSString *oAuthURL = @"https://oauth.groupme.com/oauth/authorize?client_id=fsmTfdnj8zqq1r3fnjB25IJ3muBt1VUYHcVc03BuZAkATsW1";
+    
+    WKWebViewConfiguration *theConfiguration = [[WKWebViewConfiguration alloc] init];
+    WKWebView *webView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:theConfiguration];
+    webView.navigationDelegate = self;
+    NSURL *nsurl=[NSURL URLWithString:oAuthURL];
+    NSURLRequest *nsrequest=[NSURLRequest requestWithURL:nsurl];
+    [webView loadRequest:nsrequest];
+    [self.view addSubview:webView];
+}
+
+-(void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    if ([navigationAction.request.URL.absoluteString containsString:@"com.melo2902.fbu-app"]) {
+        decisionHandler(WKNavigationActionPolicyCancel);
+        
+        NSURLComponents *urlComponents = [NSURLComponents componentsWithURL:navigationAction.request.URL
+                                                resolvingAgainstBaseURL:NO];
+        NSArray *queryItems = urlComponents.queryItems;
+        NSString *oAuthToken = [self valueForKey:@"access_token" fromQueryItems:queryItems];
+        [MTDAPIManager setAuthToken:oAuthToken];
+        [self grabUserData];
+        
+        [self performSegueWithIdentifier:@"showFilterConvoSegue" sender:nil];
+    } else {
+        decisionHandler(WKNavigationActionPolicyAllow);
+    }
+}
+
+- (NSString *)valueForKey:(NSString *)key
+           fromQueryItems:(NSArray *)queryItems {
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name=%@", key];
+    NSURLQueryItem *queryItem = [[queryItems
+                                  filteredArrayUsingPredicate:predicate]
+                                 firstObject];
+    return queryItem.value;
+}
+
+- (void)grabUserData {
+    NSMutableString *URLString = [[NSMutableString alloc] init];
+    [URLString appendString:@"https://api.groupme.com/v3/users/me?token="];
+    [URLString appendString:[MTDAPIManager returnAuthToken]];
+
+    NSError* error = nil;
+    NSData* data = [NSData dataWithContentsOfURL:[NSURL URLWithString:URLString] options:NSDataReadingUncached error:&error];
+    NSDictionary *userData = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+
+    [self updateUser: @"GroupMe" withData: userData];
+}
+
+-(void) updateUser: (NSString*) name withData: (NSDictionary *) userData {
+    MTDPlatform *newPlatform = [[MTDPlatform alloc] initWithJSONData:userData onPlatform: name];
+    PFUser *user = PFUser.currentUser;
+    user[name] = newPlatform;
+    
+    [user saveInBackground];
 }
 
 @end
