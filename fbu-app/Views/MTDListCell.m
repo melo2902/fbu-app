63c36fec329abe30051eb13ea33226d199cb0b4a
Melody Wen <40778015+melo2902@users.noreply.github.com>
2021-08-04 12:17:47 -0400

    Remove white spacing

--- fbu-app/Views/MTDListCell.m
+++ fbu-app/Views/MTDListCell.m
@@ -17,18 +17,4 @@
     [super setSelected:selected animated:animated];
 }
 
-- (void)layoutSubviews {
-    self.contentView.backgroundColor = [UIColor systemGroupedBackgroundColor];
-
-    UIView *whiteRoundedView = [[UIView alloc]initWithFrame:CGRectMake(0, 2, self.contentView.frame.size.width - 1, self.contentView.frame.size.height - 5)];
-                                      
-    whiteRoundedView.layer.backgroundColor = [UIColor whiteColor].CGColor;
-    whiteRoundedView.clipsToBounds = true;
-    whiteRoundedView.layer.masksToBounds = true;
-    whiteRoundedView.layer.cornerRadius = 5.0;
-
-    [self.contentView addSubview:whiteRoundedView];
-    [self.contentView sendSubviewToBack:whiteRoundedView];
-}
-
 @end
