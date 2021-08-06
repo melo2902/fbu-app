523a6dd64fac5d9051e9f269591ebcd4ee7087c9
Melody Wen <40778015+melo2902@users.noreply.github.com>
2021-08-05 22:03:14 -0400

    Remove comments from AppDelegate

--- fbu-app/AppDelegate.h
+++ fbu-app/AppDelegate.h
@@ -8,6 +8,5 @@
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
-@property (strong, nonatomic) UIWindow *window;
 @end
 
--- fbu-app/AppDelegate.m
+++ fbu-app/AppDelegate.m
@@ -16,10 +16,7 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    
     UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-
     UNAuthorizationOptions options = UNAuthorizationOptionAlert + UNAuthorizationOptionSound + UNAuthorizationOptionBadge + UNAuthorizationOptionCarPlay;
     
     [center requestAuthorizationWithOptions:options
@@ -32,21 +29,10 @@
     return YES;
 }
 
-
 #pragma mark - UISceneSession lifecycle
 
-
 - (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
     return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
-}
-
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 }
 
 @end
